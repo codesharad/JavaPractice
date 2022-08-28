f1ba87ad9ea97f38c890516c0c2fa2ff8b7fcbd1
codesharad <78854297+codesharad@users.noreply.github.com>
2022-08-28 12:49:26 +0530

    Update and rename HCF3.java to HCF.java

--- HCF.java
+++ HCF.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HCF3 {
+public class HCF {
     public static void main(String...args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
